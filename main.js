ca67dea673901d61693789bc124bb1db2b9c687b
gianluigi7 <gianluigiferraioli7@gmail.com>
2024-05-29 00:05:25 +0200

    .

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
 'use strict'
+
+const {createApp} = Vue;
+
+createApp({
+    data() {
+        return{
+           email: [],
+
+        };
+    },
+    created() {
+        for (let i = 0; i < 10; i++) {
+
+            axios
+            .get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response) => {
+                this.email.push(response.data.response);
+                
+            });
+        }
+    },
+}).mount('#app');
